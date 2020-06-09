03b11b59afdf426317a77e2b321bebc9e0c9e274
Guillaume R <guillaume.rondon@gmail.com>
2020-06-09 20:13:23 +0200

    Removed generated code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngmistral'`, () => {
+  it(`should have as title 'Mistral'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngmistral');
+    expect(app.title).toEqual('Mistral');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngmistral app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'Mistral app is running!'
+    );
   });
 });
